21484701cfa0e006052b4150f2c043a867e353eb
Bastian Blank <bastian.blank@credativ.de>
2020-07-06 13:24:26 +0200

    Define some public ip as static

--- modules/management/network.tf
+++ modules/management/network.tf
@@ -12,7 +12,7 @@ resource "azurerm_public_ip" "jump" {
   location            = var.location
   resource_group_name = var.resource_group_name
 
-  allocation_method = "Dynamic"
+  allocation_method = "Static"
   ip_version        = "IPv4"
 
   lifecycle {
--- modules/mirror_sync/lb.tf
+++ modules/mirror_sync/lb.tf
@@ -5,7 +5,7 @@ resource "azurerm_public_ip" "v4" {
   location            = var.location
   resource_group_name = var.resource_group_name
 
-  allocation_method = "Dynamic"
+  allocation_method = "Static"
   ip_version        = "IPv4"
 
   lifecycle {
