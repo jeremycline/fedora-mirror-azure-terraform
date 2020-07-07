9c7fcce458c1fea8e19ec920c0e8a134bc03baab
Bastian Blank <bastian.blank@credativ.de>
2020-07-07 09:48:04 +0200

    Setup network security group for backends

--- modules/mirror_backend/network.tf
+++ modules/mirror_backend/network.tf
@@ -9,3 +9,38 @@ resource "azurerm_subnet" "mirror" {
     cidrsubnet(i, 4, 0)
   ]
 }
+
+resource "azurerm_network_security_group" "mirror" {
+  name                = "mirror-backend-${var.location}"
+  location            = var.location
+  resource_group_name = var.resource_group_name
+
+  security_rule {
+    name                       = "AllowHttpBackend"
+    priority                   = 100
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = 8080
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
+
+  security_rule {
+    name                       = "AllowRsync"
+    priority                   = 101
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = 873
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
+}
+
+resource "azurerm_subnet_network_security_group_association" "mirror" {
+  subnet_id                 = azurerm_subnet.mirror.id
+  network_security_group_id = azurerm_network_security_group.mirror.id
+}
