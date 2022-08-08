5e650d86a6a6eaaad45a971ce40caf86675ca6e1
Bastian Blank <bastian.blank@credativ.de>
2022-08-08 14:47:02 +0200

    Replace deprecated resource

--- modules/mirror_backend/trafficmanager.tf
+++ modules/mirror_backend/trafficmanager.tf
@@ -1,20 +1,26 @@
 # SPDX-License-Identifier: GPL-3.0-or-later
 
-resource "azurerm_traffic_manager_endpoint" "mirror" {
+data "azurerm_traffic_manager_profile" "mirror" {
   for_each = var.trafficmanager_profile_names
 
-  name                = "${var.resource_group_name}-${var.location}"
+  name                = each.value
   resource_group_name = var.trafficmanager_resource_group_name
-  profile_name        = each.value
+}
 
-  type               = "azureEndpoints"
+resource "azurerm_traffic_manager_azure_endpoint" "mirror" {
+  for_each = var.trafficmanager_profile_names
+
+  name       = "${var.resource_group_name}-${var.location}"
+  profile_id = data.azurerm_traffic_manager_profile.mirror[each.value].id
+
   target_resource_id = azurerm_public_ip.v4.id
+  weight             = 100
 
-  endpoint_status = "Disabled"
+  enabled = false
 
   lifecycle {
     ignore_changes = [
-      endpoint_status,
+      enabled,
     ]
   }
 }
