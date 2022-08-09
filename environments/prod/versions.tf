1f0cd8e56fe1a41886db8bf3c42959232a68abc5
Bastian Blank <bastian.blank@credativ.de>
2022-08-09 13:26:28 +0200

    Upgrade modules

--- environments/prod/versions.tf
+++ environments/prod/versions.tf
@@ -2,10 +2,4 @@
 
 terraform {
   required_version = ">= 1.2"
-  required_providers {
-    azurerm = {
-      source  = "hashicorp/azurerm"
-      version = "~> 2.99"
-    }
-  }
 }
--- modules/mirror_backend/versions.tf
+++ modules/mirror_backend/versions.tf
@@ -1,5 +1,11 @@
 # SPDX-License-Identifier: GPL-3.0-or-later
 
 terraform {
-  required_version = ">= 0.13"
+  required_version = ">= 1.2"
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 3.0"
+    }
+  }
 }
