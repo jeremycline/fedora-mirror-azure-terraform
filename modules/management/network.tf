d0932ba13e0f24dc7fc44d4d3eb3ed7e219c8b29
Bastian Blank <bastian.blank@credativ.de>
2022-08-10 13:05:06 +0200

    Use Standard sku simple ip addresses

--- modules/management/network.tf
+++ modules/management/network.tf
@@ -16,6 +16,7 @@ resource "azurerm_public_ip" "jump" {
   location            = var.location
   resource_group_name = var.resource_group_name
 
+  sku               = "Standard"
   allocation_method = "Static"
   ip_version        = "IPv4"
 
