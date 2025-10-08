ae0033a725bdf8c316ad943e3503e30dfa39fc6a
Jeremy Cline <jeremycline@linux.microsoft.com>
2025-10-08 11:49:51 -0400

    fedora: Adjust resource group and storage account names

--- environments/prod/backend.tf
+++ environments/prod/backend.tf
@@ -1,10 +1,10 @@
 terraform {
   backend "azurerm" {
     subscription_id      = "f84e5905-7ce0-4716-9d3a-ddd48f487cf4"
-    resource_group_name  = "debian-mirror"
-    storage_account_name = "debianmirrorterraform"
+    resource_group_name  = "fedora-mirror"
+    storage_account_name = "fedoramirrorterraform"
     use_azuread_auth     = true
-    container_name       = "v3-prod"
+    container_name       = "v1-prod"
     key                  = "terraform.tfstate"
   }
 }
