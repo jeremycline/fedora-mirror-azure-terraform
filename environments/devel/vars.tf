8595b6b0ad71aa0b5d6b02e18647c373277a32a2
Jeremy Cline <jeremycline@linux.microsoft.com>
2025-10-08 16:33:27 -0400

    fedora: change regions to less full places
    It's very difficult to get westeurope VMs allocated

--- environments/devel/vars.tf
+++ environments/devel/vars.tf
@@ -22,16 +22,16 @@ locals {
   network_base_v6 = "fe00:632b:2465:100::/52"
 
   networks_hub = {
-    "westeurope" = 0
+    "eastus2" = 0
   }
 
   networks_mirror = {
-    "westeurope" = 4
-    "westus2"    = 5
+    "eastus2" = 4
+    "westus2" = 5
   }
 
-  location_management = "westeurope"
-  location_sync       = "westeurope"
+  location_management = "eastus2"
+  location_sync       = "eastus2"
 
   # Required but unused ssh key
   ssh_key = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDOm05Ms4RxPGHrSqXu1UvO/f/b8BOSRlZYYGqhaVIrjk1ObR7OC67HRMPxi4y+ILwRve87OQ2jpmM4E16RB/fNpPjnsBLPI1zYDnhRwAzPVRbubxA5DW/W9WbKR62UqRKrzxg68a2yeFnkmVJS0g3I+6V9YTZtlL61nytRiZnA+327sE0nSAh9GPqZkDFPy1PeS8mqjqO6Y5ATSjfSopdqnZ5O0cUXu8dMvZpnVSwKN74Kgd0DGFk9GTEWtse9/xY8HXPmru8mbZnEfuYMKR5xaBBEluDpFHwzyOdI5DocaOREUZCxU+7AY7eeeKszpujOQgChwT+pQxvfmL7XpOqf"
