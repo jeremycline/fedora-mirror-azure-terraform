d150402ed4280c55d8bc1cfff71f12110e822f1e
Jeremy Cline <jeremycline@linux.microsoft.com>
2025-10-22 09:37:16 -0400

    fedora: Update azurerm to recent version and fix deprecation warnings

--- environments/prod/provider.tf
+++ environments/prod/provider.tf
@@ -3,8 +3,6 @@
 provider "azurerm" {
   subscription_id = "8ddde9ba-f5e7-4a4f-a9a1-64181b250697"
 
-  skip_provider_registration = true
-
-  features {
-  }
+  resource_provider_registrations = "none"
+  features {}
 }
